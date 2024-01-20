fe578ab75cc427967dd6f81ac5b2eb0f3c6d8a52
v-m-dev <morozvitalii2403@gmail.com>
2024-01-20 19:54:28 +0200

    feat: add assets to project-dist

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,6 +6,8 @@ const path = require('path');
   const indexHtmlPath = path.join(projectDistPath, 'index.html');
   const indexHtml = await composeIndexHtml();
   const styles = await collectStyles(path.join(__dirname, 'styles'));
+  const sorceAssetsPath = path.join(__dirname, 'assets');
+  const targetAssetsPath = path.join(projectDistPath, 'assets');
 
   //   Create project-dist folder
   await createFolder(projectDistPath);
@@ -22,17 +24,14 @@ const path = require('path');
   } catch (err) {
     console.log(err.message);
   }
-})();
 
-async function getFileContent(filePath) {
-  let fileContent = '';
-  try {
-    fileContent = await fsPromises.readFile(filePath, 'utf8');
-  } catch (err) {
-    console.log(err.message);
-  }
-  return fileContent;
-}
+  //   Reset project-dist/assets folder if it exists to remove unneeded files
+  await removeFolder(targetAssetsPath);
+  await createFolder(targetAssetsPath);
+
+  //   Copy assets to project-dist/assets folder
+  await copyFolder(sorceAssetsPath, targetAssetsPath);
+})();
 
 async function createFolder(folderPath) {
   let folder = null;
@@ -46,6 +45,39 @@ async function createFolder(folderPath) {
   return folder;
 }
 
+async function copyFolder(sorcePath, targetPath) {
+  try {
+    const files = await fsPromises.readdir(sorcePath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const sorceFilePath = path.join(sorcePath, file.name);
+        const targetFilePath = path.join(targetPath, file.name);
+        await fsPromises.copyFile(sorceFilePath, targetFilePath);
+      } else {
+        await createFolder(path.join(targetPath, file.name));
+        await copyFolder(
+          path.join(sorcePath, file.name),
+          path.join(targetPath, file.name),
+        );
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function removeFolder(folderPath) {
+  let isNewFolder = false;
+
+  if (isNewFolder) {
+    try {
+      await fsPromises.rm(folderPath, { recursive: true, force: true });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+}
+
 async function createFile(filePath, fileContent) {
   let file = null;
   try {
@@ -54,6 +86,31 @@ async function createFile(filePath, fileContent) {
     console.log(err.message);
   }
   return file;
+}
+
+async function getFileContent(filePath) {
+  let fileContent = '';
+  try {
+    fileContent = await fsPromises.readFile(filePath, 'utf8');
+  } catch (err) {
+    console.log(err.message);
+  }
+  return fileContent;
+}
+
+async function collectStyles(stylesPath) {
+  const styles = [];
+
+  const files = await fsPromises.readdir(stylesPath);
+
+  // Read all css files in the styles folder and push their content to the stylesToBundle array.
+  for (const file of files) {
+    const filePath = path.join(stylesPath, file);
+    const data = await fsPromises.readFile(filePath, 'utf8');
+    styles.push(data);
+  }
+
+  return styles.join('\n');
 }
 
 async function composeIndexHtml() {
@@ -74,19 +131,4 @@ async function composeIndexHtml() {
   templateContent = templateContent.replace('{{footer}}', footerContent);
 
   return templateContent;
-}
-
-async function collectStyles(stylesPath) {
-  const styles = [];
-
-  const files = await fsPromises.readdir(stylesPath);
-
-  // Read all css files in the styles folder and push their content to the stylesToBundle array.
-  for (const file of files) {
-    const filePath = path.join(stylesPath, file);
-    const data = await fsPromises.readFile(filePath, 'utf8');
-    styles.push(data);
-  }
-
-  return styles.join('\n');
 }
