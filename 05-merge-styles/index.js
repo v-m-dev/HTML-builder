87933cede53d30691a3bba4cd77dade1dba00372
v-m-dev <morozvitalii2403@gmail.com>
2024-01-21 12:28:31 +0200

    refactor: add error handling to merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,12 +2,16 @@
   const path = require('path');
   const fsPromises = require('fs/promises');
   const pathToStylesFolder = path.join(__dirname, 'styles');
-
+  let files = null;
   const stylesToBundle = [];
 
-  const files = await fsPromises.readdir(pathToStylesFolder, {
-    withFileTypes: true,
-  });
+  try {
+    files = await fsPromises.readdir(pathToStylesFolder, {
+      withFileTypes: true,
+    });
+  } catch (error) {
+    console.log('cannot read styles folder');
+  }
 
   // Read all CSS files in the styles folder and push their content to the stylesToBundle array.
   for (const file of files) {
@@ -16,16 +20,23 @@
 
       const fileExtension = path.extname(filePath);
       if (fileExtension === '.css') {
-        const data = await fsPromises.readFile(filePath, 'utf8');
-
-        stylesToBundle.push(data);
+        try {
+          const data = await fsPromises.readFile(filePath, 'utf8');
+          stylesToBundle.push(data);
+        } catch (error) {
+          console.log('cannot read file ' + file.name);
+        }
       }
     }
   }
 
   //   Write the stylesToBundle array to a new file called bundle.css.
-  await fsPromises.writeFile(
-    path.join(__dirname, 'project-dist/bundle.css'),
-    stylesToBundle.join('\n'),
-  );
+  try {
+    await fsPromises.writeFile(
+      path.join(__dirname, 'project-dist/bundle.css'),
+      stylesToBundle.join('\n'),
+    );
+  } catch (error) {
+    console.log('cannot add bundle.css to project-dist folder');
+  }
 })();
