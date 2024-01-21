6ec7ff375dbde400cda772c000784f6bc3422801
v-m-dev <morozvitalii2403@gmail.com>
2024-01-21 12:07:32 +0200

    fix: remove bugs from copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -13,7 +13,6 @@
     console.log('files-copy folder exists!');
   } catch (err) {
     console.log('There is no files-copy folder!');
-    throw err;
   }
 
   //if files-copy folder exists delete it with all its files
@@ -23,7 +22,6 @@
       console.log('file-copy folder deleted!');
     } catch (err) {
       console.log('file-copy folder not found or already deleted!');
-      throw err;
     }
   }
 
@@ -33,20 +31,19 @@
     console.log('file-copy folder created!');
   } catch (err) {
     console.log('file-copy folder has not been created!');
-    throw err;
   }
 
   // copy files from files folder to files-copy folder.
   fs.readdir(oldFolderPath, (err, files) => {
     if (err) {
-      throw err;
+      console.log('cannot read file-copy!');
     }
     files.forEach((file) => {
       const oldFilePath = path.join(oldFolderPath, file);
       const newFilePath = path.join(newFolderPath, file);
       fs.copyFile(oldFilePath, newFilePath, (err) => {
         if (err) {
-          throw err;
+          console.log(file + ' has been coppied');
         }
         console.log(file + ' has been coppied');
       });
