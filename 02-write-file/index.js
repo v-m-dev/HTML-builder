6f5aa7a984c90923eb8b368ed79d76ac22e42de7
v-m-dev <morozvitalii2403@gmail.com>
2024-01-24 08:36:47 +0200

    fix: remove bugs from write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const textPath = path.join(__dirname, 'text.txt');
 
+fs.writeFile(textPath, '', function (err) {
+  if (err) throw err;
+});
+
 const rl = readline.createInterface(process.stdin, process.stdout);
 console.log('Hello there! Do you whant to add something to text.txt file?');
 
@@ -10,13 +14,18 @@ rl.setPrompt('Add any text or type "exit" to close the program: ');
 rl.prompt();
 rl.on('line', (userInput) => {
   if (userInput.toLowerCase() === 'exit') {
-    rl.close();
-    console.log('See you later!');
-    rl.removeAllListeners();
+    process.exit();
     return;
   }
 
   fs.appendFile(textPath, `${userInput}\n`, function (err) {
     if (err) throw err;
   });
+});
+
+process.on('exit', function () {
+  rl.close();
+  console.log('See you later!');
+  rl.removeAllListeners();
+  return;
 });
